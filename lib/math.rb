66450b93c587c79c0a881d66c101e54361a2af50
Brianna Fetterley <bnfetterley@usfca.edu>
2019-09-22 23:44:32 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -5,26 +5,26 @@ end
 
 addition (9,9)
 
-def subtraction(num1, num2)
-difference = num1 - num2
-print remainder
-end
+#def subtraction(num1, num2)
+#difference = num1 - num2
+#print remainder
+#end
 
-difference (9,9)
+#difference (9,9)
 
-def division(num1, num2)
-quotient = num1 / num2
-print quotient
-end
+#def division(num1, num2)
+#quotient = num1 / num2
+#print quotient
+#end
 
-quotient(9,9)
+#quotient(9,9)
 
-def multiplication(num1, num2)
-product = num1 * num2
-print product
-end
+#def multiplication(num1, num2)
+#product = num1 * num2
+#print product
+#end
 
-product(9,9)
+#product(9,9)
 
 def modulo(num1, num2)
 remainder = num1 % num2
