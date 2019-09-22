87fd9a94f5ae45a8b192eb9a4f246f062f68e6bd
Brianna Fetterley <bnfetterley@usfca.edu>
2019-09-22 23:42:18 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,24 +3,34 @@ sum = num1 + num2
 print sum
 end
 
+addition (9,9)
+
 def subtraction(num1, num2)
-difference = num1 + num2
+difference = num1 - num2
 print remainder
 end
+
+difference (9,9)
 
 def division(num1, num2)
 quotient = num1 / num2
 print quotient
 end
 
+quotient(9,9)
+
 def multiplication(num1, num2)
 product = num1 * num2
 print product
 end
 
+product(9,9)
+
 def modulo(num1, num2)
 remainder = num1 % num2
 end
+
+modulo = (9,9)
 
 def square_root(num)
 Math.sqrt(9)
